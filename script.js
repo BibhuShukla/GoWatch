748cb9e5f41813e72127eee50ee6e66708b458de
Bibhu Shukla <bibhushukla24@navgurukul.org>
2025-02-22 17:20:00 +0530

    GoWatch

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const categories = document.querySelectorAll(".category");
+
+    categories.forEach(category => {
+        category.addEventListener("click", () => {
+            alert(`You selected ${category.innerText} category!`);
+        });
+    });
+});
